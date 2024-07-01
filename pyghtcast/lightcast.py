4fdb710bec291b0386c4a8b30a8e61c11d75ab69
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-07-01 14:03:33 -0500

    lightclass

--- pyghtcast/lightcast.py
+++ pyghtcast/lightcast.py
@@ -1,20 +1,20 @@
 import lightcast_api.coreLmi as corelmi
 import pandas as pd
 
-conn = corelmi.CoreLMIConnection()
+class Lightcast:
+    conn = corelmi.CoreLMIConnection()
+    def __init__(self):
+        pass
 
-def build_query_corelmi(cols: list, constraints: list[dict] = []) -> dict:
-    query: dict = {"metrics": [], "constraints": constraints}
+    def build_query_corelmi(self, cols: list, constraints: list[dict] = []) -> dict:
+        query: dict = {"metrics": [], "constraints": constraints}
 
-    # take as list of column names just to make the syntax easier
-    # convert it to expected syntax for the API
-    for c in cols:
-        query["metrics"].append({"name": c})
+        # take as list of column names just to make the syntax easier
+        # convert it to expected syntax for the API
+        for c in cols:
+            query["metrics"].append({"name": c})
 
-    return query
+        return query
 
-
-def query_corelmi(dataset: str, query: dict, datarun: str = "2024.2") -> pd.DataFrame:
-    return conn.post_retrieve_df(dataset, query, datarun)
-
-
+    def query_corelmi(self, dataset: str, query: dict, datarun: str = "2024.2") -> pd.DataFrame:
+        return self.conn.post_retrieve_df(dataset, query, datarun)
